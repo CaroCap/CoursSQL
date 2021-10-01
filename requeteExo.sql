8e808da946971a385f5d084e683895fa5e6eba5d
CaroCap <carolinecap.event@gmail.com>
2021-10-01 10:30:45 +0200

    exercices requetes 21

--- requeteExo.sql
+++ requeteExo.sql
@@ -2,7 +2,7 @@
 
 -- ## Requêtes simples
 -- 1.  Obtenir toutes les informations de tous les films
-SELECT * FROM `film`;
+SELECT * FROM film;
 
 -- 2.  Obtenir une liste des noms des pays
 SELECT country FROM country;
@@ -46,24 +46,30 @@ SELECT title FROM film WHERE title LIKE 'b%' OR title LIKE 'd%';
 SELECT * FROM customer WHERE last_name = 'Moore';
 
 -- 15. Chercher tous les clients qui s'appellent « Rashomon »
+-- ?
 SELECT * FROM customer WHERE last_name = 'Rashomon';
 
 -- 16. Chercher tous les paiements qui ont été réalisés pendant les 10 premiers jours d'Aout 2005
+--?
+ SELECT * FROM payment WHERE payment_date BETWEEN 2005-08-01 AND 2005-08-10;
 
 -- ## Agrégation
-
 -- 17. Obtenir le nombre de clients
+SELECT COUNT(*) FROM customer;
 
 -- 18. Calculer la moyenne des prix de location
+/*?*/ SELECT AVG(amount) from payment;
 
 -- 19. Compter le nombre de clients actifs
+SELECT COUNT(active) FROM `customer` WHERE active = 1;
 
 -- 20. Obtenir la durée du film le plus long
+SELECT MAX(length) FROM film;
 
 -- 21. Calculer la durée moyenne des films
+SELECT AVG(length) FROM film;
 
 -- ## Requêtes de jointure
-
 -- 22. Obtenir la ville de chaque client
 
 -- 23. Obtenir tous les titres des films joués par Fred Costner
@@ -108,10 +114,7 @@ SELECT * FROM customer WHERE last_name = 'Rashomon';
 --     locations
 
 -- ## Calculs et fonctions de base
-
--- 41. Les prix de location sont en dollars. Obtenez les informations du
---     tableau de « payments » en rajoutant une colonne contenant les prix
---     en euros
+-- 41. Les prix de location sont en dollars. Obtenez les informations du tableau de « payments » en rajoutant une colonne contenant les prix en euros
 
 -- 42. Obtenir les films dont le titre a plus de 10 caractères
 
@@ -122,8 +125,6 @@ SELECT * FROM customer WHERE last_name = 'Rashomon';
 -- 45. Obtenir tous les prix de location arrondis ver le haut
 
 -- ## Exercice
-
 -- 46. Créez 3 requêtes de jointure ayant au moins 3 tableaux (INNER JOIN)
 
--- 47. Créez 3 requêtes de jointure ayant au moins 3 tableaux et qui
---     contiennent un regroupement
+-- 47. Créez 3 requêtes de jointure ayant au moins 3 tableaux et qui contiennent un regroupement
